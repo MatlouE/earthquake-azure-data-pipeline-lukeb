834fa7b5719e7a75da63b52c8fdf4260c6ccb6d0
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-01-19 17:51:22 +0000

    modified:   Notebooks/copy.py 	modified:   Notebooks/silver.ipynb

--- Notebooks/copy.py
+++ Notebooks/copy.py
@@ -1,88 +1,104 @@
 
-# Mount ADLS Gen2
-# Required each time the cluster is restarted which should be only on the first notebook as they run in order
 
-tiers = ["bronze", "silver", "gold"]
-adls_paths = {tier: f"abfss://{tier}@earthquakedataluke.dfs.core.windows.net/" for tier in tiers}
-
-# Accessing paths
-bronze_adls = adls_paths["bronze"]
-silver_adls = adls_paths["silver"]
-gold_adls = adls_paths["gold"] 
-
-dbutils.fs.ls(bronze_adls)
-dbutils.fs.ls(silver_adls)
-dbutils.fs.ls(gold_adls)
-     
-
-''' Data Factory
-# Get base parameters
-dbutils.widgets.text("start_date", "")
-dbutils.widgets.text("end_date", "")
-start_date = dbutils.widgets.get("start_date")
-end_date = dbutils.widgets.get("end_date")
-'''
-     
-
-import requests
-import json
 from datetime import date, timedelta
-     
 
 # Remove this before running Data Factory Pipeline
 start_date = date.today() - timedelta(1)
-end_date = date.today()
-     
 
-start_date, end_date
-     
-
-# Construct the API URL with start and end dates provided by Data Factory, formatted for geojson output.
-url = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_date}&endtime={end_date}"
-
-try:
-    # Make the GET request to fetch data
-    response = requests.get(url)
-
-    # Check if the request was successful
-    response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
-    data = response.json().get('features', [])
-
-    if not data:
-        print("No data returned for the specified date range.")
-    else:
-        # Specify the ADLS path
-        file_path = f"{bronze_adls}/{start_date}_earthquake_data.json"
-
-        # Save the JSON data
-        json_data = json.dumps(data, indent=4)
-        dbutils.fs.put(file_path, json_data, overwrite=True)
-        print(f"Data successfully saved to {file_path}")
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching data from API: {e}")
-
-     
-
-data[0]
+bronze_adls = "abfss://bronze@earthquakedataluke.dfs.core.windows.net/"
+silver_adls = "abfss://silver@earthquakedataluke.dfs.core.windows.net/"
      
 
 ''' Data Factory
-# Define your variables
-output_data = {
-    "start_date": start_date,
-    "end_date": end_date,
-    "bronze_adls": bronze_adls,
-    "silver_adls": silver_adls,
-    "gold_adls": gold_adls
-}
+import json
 
-# Serialize the dictionary to a JSON string
-output_json = json.dumps(output_data)
+# Retrieve the bronze_params directly as a widget
+bronze_params = dbutils.widgets.get("bronze_params")
+print(f"Raw bronze_params: {bronze_params}")
 
-# Log the serialized JSON for debugging
-print(f"Serialized JSON: {output_json}")
+# Parse the JSON string
+output_data = json.loads(bronze_params)
 
-# Return the JSON string
-dbutils.notebook.exit(output_json)
+# Access individual variables
+start_date = output_data.get("start_date", "")
+end_date = output_data.get("end_date", "")
+bronze_adls = output_data.get("bronze_adls", "")
+silver_adls = output_data.get("silver_adls", "")
+gold_adls = output_data.get("gold_adls", "")
+
+print(f"Start Date: {start_date}, Bronze ADLS: {bronze_adls}")
+'''
+     
+
+from pyspark.sql.functions import col, isnull, when
+from pyspark.sql.types import TimestampType
+from datetime import date, timedelta
+     
+
+# Load the JSON data into a Spark DataFrame
+df = spark.read.option("multiline", "true").json(f"{bronze_adls}{start_date}_earthquake_data.json")
+     
+
+df
+     
+
+df.head()
+     
+
+# Reshape earthquake data
+df = (
+    df
+    .select(
+        'id',
+        col('geometry.coordinates').getItem(0).alias('longitude'),
+        col('geometry.coordinates').getItem(1).alias('latitude'),
+        col('geometry.coordinates').getItem(2).alias('elevation'),
+        col('properties.title').alias('title'),
+        col('properties.place').alias('place_description'),
+        col('properties.sig').alias('sig'),
+        col('properties.mag').alias('mag'),
+        col('properties.magType').alias('magType'),
+        col('properties.time').alias('time'),
+        col('properties.updated').alias('updated')
+    )
+)
+     
+
+df
+     
+
+df.head()
+     
+
+# Validate data: Check for missing or null values
+df = (
+    df
+    .withColumn('longitude', when(isnull(col('longitude')), 0).otherwise(col('longitude')))
+    .withColumn('latitude', when(isnull(col('latitude')), 0).otherwise(col('latitude')))
+    .withColumn('time', when(isnull(col('time')), 0).otherwise(col('time')))
+)
+     
+
+# Convert 'time' and 'updated' to timestamp from Unix time
+df = (
+    df
+    .withColumn('time', (col('time') / 1000).cast(TimestampType()))
+    .withColumn('updated', (col('updated') / 1000).cast(TimestampType()))
+)
+     
+
+df.head()
+     
+
+# Save the transformed DataFrame to the Silver container
+silver_output_path = f"{silver_adls}earthquake_events_silver/"
+     
+
+# Append DataFrame to Silver container in Parquet format
+df.write.mode('append').parquet(silver_output_path)
+     
+
+''' Data Factory
+dbutils.notebook.exit(silver_output_path)
 '''
      

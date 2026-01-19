7556a180dc1ffde063f9cd8bf05510824bdfc193
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-01-19 18:21:00 +0000

    modified:   Notebooks/copy.py 	modified:   Notebooks/gold.ipynb

--- Notebooks/copy.py
+++ Notebooks/copy.py
@@ -5,100 +5,118 @@ from datetime import date, timedelta
 # Remove this before running Data Factory Pipeline
 start_date = date.today() - timedelta(1)
 
-bronze_adls = "abfss://bronze@earthquakedataluke.dfs.core.windows.net/"
 silver_adls = "abfss://silver@earthquakedataluke.dfs.core.windows.net/"
+gold_adls = "abfss://gold@earthquakedataluke.dfs.core.windows.net/"
+
+silver_data = f"{silver_adls}earthquake_events_silver/"
      
 
 ''' Data Factory
+
 import json
 
-# Retrieve the bronze_params directly as a widget
+# Get base parameters
+dbutils.widgets.text("bronze_params", "")
+dbutils.widgets.text("silver_params", "")
+
 bronze_params = dbutils.widgets.get("bronze_params")
+silver_params = dbutils.widgets.get("silver_params")
+
+# Debug: Print the raw input values for troubleshooting
 print(f"Raw bronze_params: {bronze_params}")
+print(f"Raw silver_params: {silver_params}")
 
 # Parse the JSON string
-output_data = json.loads(bronze_params)
+bronze_data = json.loads(bronze_params)
 
 # Access individual variables
-start_date = output_data.get("start_date", "")
-end_date = output_data.get("end_date", "")
-bronze_adls = output_data.get("bronze_adls", "")
-silver_adls = output_data.get("silver_adls", "")
-gold_adls = output_data.get("gold_adls", "")
+start_date = bronze_data.get("start_date", "")
+end_date = bronze_data.get("end_date", "")
+silver_adls = bronze_data.get("silver_adls", "")
+gold_adls = bronze_data.get("gold_adls", "")
+silver_data = silver_params
 
-print(f"Start Date: {start_date}, Bronze ADLS: {bronze_adls}")
+# Debug: Print the extracted values for verification
+print(f"Start Date: {start_date}, End Date: {end_date}")
+print(f"Silver ADLS Path: {silver_adls}, Gold ADLS Path: {gold_adls}")
 '''
      
 
-from pyspark.sql.functions import col, isnull, when
-from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import when, col, udf
+from pyspark.sql.types import StringType
+# Ensure the below library is installed on your cluster
+import reverse_geocoder as rg
 from datetime import date, timedelta
      
 
-# Load the JSON data into a Spark DataFrame
-df = spark.read.option("multiline", "true").json(f"{bronze_adls}{start_date}_earthquake_data.json")
+df = spark.read.parquet(silver_data).filter(col('time') > start_date)
      
 
-df
+df = df.limit(100) # added to speed up processings as during testing it was proving a bottleneck
+# The problem is caused by the Python UDF (reverse_geocoder) being a bottleneck due to its non-parallel nature and high computational cost per task
      
 
-df.head()
+def get_country_code(lat, lon):
+    """
+    Retrieve the country code for a given latitude and longitude.
+
+    Parameters:
+    lat (float or str): Latitude of the location.
+    lon (float or str): Longitude of the location.
+
+    Returns:
+    str: Country code of the location, retrieved using the reverse geocoding API.
+
+    Example:
+    >>> get_country_details(48.8588443, 2.2943506)
+    'FR'
+    """
+    try:
+        coordinates = (float(lat), float(lon))
+        result = rg.search(coordinates)[0].get('cc')
+        print(f"Processed coordinates: {coordinates} -> {result}")
+        return result
+    except Exception as e:
+        print(f"Error processing coordinates: {lat}, {lon} -> {str(e)}")
+        return None
      
 
-# Reshape earthquake data
-df = (
-    df
-    .select(
-        'id',
-        col('geometry.coordinates').getItem(0).alias('longitude'),
-        col('geometry.coordinates').getItem(1).alias('latitude'),
-        col('geometry.coordinates').getItem(2).alias('elevation'),
-        col('properties.title').alias('title'),
-        col('properties.place').alias('place_description'),
-        col('properties.sig').alias('sig'),
-        col('properties.mag').alias('mag'),
-        col('properties.magType').alias('magType'),
-        col('properties.time').alias('time'),
-        col('properties.updated').alias('updated')
-    )
-)
+# registering the udfs so they can be used on spark dataframes
+get_country_code_udf = udf(get_country_code, StringType())
      
 
-df
+get_country_code(48.8588443, 2.2943506)
      
 
-df.head()
+# adding country_code and city attributes
+df_with_location = \
+                df.\
+                    withColumn("country_code", get_country_code_udf(col("latitude"), col("longitude")))
      
 
-# Validate data: Check for missing or null values
-df = (
-    df
-    .withColumn('longitude', when(isnull(col('longitude')), 0).otherwise(col('longitude')))
-    .withColumn('latitude', when(isnull(col('latitude')), 0).otherwise(col('latitude')))
-    .withColumn('time', when(isnull(col('time')), 0).otherwise(col('time')))
-)
+df.printSchema()
      
 
-# Convert 'time' and 'updated' to timestamp from Unix time
-df = (
-    df
-    .withColumn('time', (col('time') / 1000).cast(TimestampType()))
-    .withColumn('updated', (col('updated') / 1000).cast(TimestampType()))
-)
+df_with_location.printSchema()
      
 
-df.head()
+# adding significance classification
+df_with_location_sig_class = \
+                            df_with_location.\
+                                withColumn('sig_class', 
+                                            when(col("sig") < 100, "Low").\
+                                            when((col("sig") >= 100) & (col("sig") < 500), "Moderate").\
+                                            otherwise("High")
+                                            )
+     
+
+df_with_location_sig_class.printSchema()
      
 
 # Save the transformed DataFrame to the Silver container
-silver_output_path = f"{silver_adls}earthquake_events_silver/"
+gold_output_path = f"{gold_adls}earthquake_events_gold/"
      
 
 # Append DataFrame to Silver container in Parquet format
-df.write.mode('append').parquet(silver_output_path)
-     
-
-''' Data Factory
-dbutils.notebook.exit(silver_output_path)
-'''
+df_with_location_sig_class.write.mode('append').parquet(gold_output_path)
      
